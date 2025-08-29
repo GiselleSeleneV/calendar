33436963971ae58df12f6a69ab97c2596e955666
Selene Vargas <dev.front3@talentum.edu.co>
2025-08-29 14:44:47 -0500

    finished integration with back

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,25 +1,70 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
+import { calendarApi } from "../api";
+import { convertEventsToDateEvents } from "../helpers";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector(state => state.calendar);
+    const { user } = useSelector(state => state.auth);
 
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent))
     };
 
     const startSavingEvent = async (calendarEvent) => {
-        if (calendarEvent._id) {
-            dispatch(onUpdateEvent({ ...calendarEvent }));
-        } else {
-            dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }))
+
+        try {
+            if (calendarEvent.id) {
+
+                await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent);
+                dispatch(onUpdateEvent({ ...calendarEvent, user }));
+
+                return;
+            };
+
+            const { data } = await calendarApi.post('/events', calendarEvent);
+            dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
+
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: error.response.data.errorMessage,
+                confirmButtonText: 'Aceptar',
+                confirmButtonColor: '#0F172A'
+            });
+        };
+    };
+
+    const startDeletingEvent = async () => {
+        try {
+            await calendarApi.delete(`/events/${activeEvent.id}`);
+            dispatch(onDeleteEvent());
+
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al eliminar evento',
+                text: error.response.data.errorMessage,
+                confirmButtonText: 'Aceptar',
+                confirmButtonColor: '#0F172A'
+            });
         }
     };
 
-    const startDeletingEvent = () => {
-        dispatch(onDeleteEvent());
-    }
+    const startLoadingEvents = async () => {
+        try {
+            const { data } = await calendarApi.get('/events');
+            const events = convertEventsToDateEvents(data.events);
+
+            dispatch(onLoadEvents(events));
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return {
         //props
@@ -30,6 +75,7 @@ export const useCalendarStore = () => {
         //metodos
         setActiveEvent,
         startSavingEvent,
-        startDeletingEvent
+        startDeletingEvent,
+        startLoadingEvents
     }
 }
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -1,23 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addHours } from 'date-fns';
-
-const tempEvent = {
-    _id: new Date().getTime(),
-    title: 'Cumpleaños del jefe',
-    notes: 'Hay que comprar el pastel',
-    start: new Date(),
-    end: addHours(new Date(), 2),
-    bgColor: '#fafafa',
-    user: {
-        _id: '123',
-        name: 'Giss'
-    }
-}
 
 export const calendarSlice = createSlice({
     name: 'calendar',
     initialState: {
-        events: [tempEvent],
+        isLoadingEvents: true,
+        events: [],
         activeEvent: null
     },
     reducers: {
@@ -33,7 +20,7 @@ export const calendarSlice = createSlice({
         onUpdateEvent: (state, { payload }) => {
 
             state.events = state.events.map(event => {
-                if (event._id === payload._id) {
+                if (event.id === payload.id) {
                     return payload;
                 }
 
@@ -43,16 +30,41 @@ export const calendarSlice = createSlice({
 
         onDeleteEvent: (state) => {
             if (state.activeEvent) {
-                state.events = state.events.filter(event => event._id !== state.activeEvent._id);
+                state.events = state.events.filter(event => event.id !== state.activeEvent.id);
                 state.activeEvent = null;
             };
         },
 
         onClearActiveEvent: (state) => {
             state.activeEvent = null;
+        },
+
+        onLoadEvents: (state, { payload = [] }) => {
+            state.isLoadingEvents = false;
+
+            payload.forEach(event => {
+                const exists = state.events.some(dbEvent => dbEvent.id === event.id);
+
+                if (!exists) {
+                    state.events.push(event);
+                };
+            });
+        },
+        onLogoutCalendar: (state) => {
+            state.isLoadingEvents = true,
+                state.events = [],
+                state.activeEvent = null
         }
 
     }
 });
 
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onClearActiveEvent } = calendarSlice.actions;
+export const {
+    onSetActiveEvent,
+    onAddNewEvent,
+    onUpdateEvent,
+    onDeleteEvent,
+    onClearActiveEvent,
+    onLoadEvents,
+    onLogoutCalendar
+} = calendarSlice.actions;
